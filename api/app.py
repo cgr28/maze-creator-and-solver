04da67260bef51c159f3499927e8d7d77907f1e4
cgr28 <cgr28@njit.edu>
2022-08-16 23:36:32 -0400

    FALSE debug

--- api/app.py
+++ api/app.py
@@ -67,4 +67,4 @@ def serve():
     return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
-    app.run(host="localhost", port=8080, debug=True)
+    app.run(host="localhost", port=8080, debug=False)
