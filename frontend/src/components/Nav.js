ad3fd50c05ce02df5a491b874ac7deac80dded62
Colbe Roberson <cgr28@njit.edu>
2022-09-25 02:24:31 -0400

    maze-pedia.com

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -4,7 +4,7 @@ import "./Nav.scss";
 export default function MainNav() {
     return (
         <nav className="mt-3 container main-nav text-center">
-            <a className="navbar-brand">Maze Creator and Solver</a>
+            <a className="navbar-brand">maze-pedia.com</a>
         </nav>
     );
 }
